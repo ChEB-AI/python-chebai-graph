1481b59118e3f7d374c1cb5e08dbb45291120337
sfluegel <sfluegel@ovgu.de>
2023-12-12 17:07:33 +0100

    add config for graph attention net

--- chebai_graph/models/graph.py
+++ chebai_graph/models/graph.py
@@ -57,15 +57,23 @@ class JCIGraphNet(ChebaiBaseNet):
 class JCIGraphAttentionNet(ChebaiBaseNet):
     NAME = "AGNN"
 
-    def __init__(self, **kwargs):
+    def __init__(self, config: typing.Dict, **kwargs):
         super().__init__(**kwargs)
-        in_length = kwargs.get("in_length")
-        hidden_length = kwargs.get("hidden_length")
+
+        in_length = config["in_length"]
+        hidden_length = config["hidden_length"]
+        dropout_rate = config["dropout_rate"]
+
         self.embedding = torch.nn.Embedding(800, in_length)
         self.edge_embedding = torch.nn.Embedding(4, in_length)
         in_length = in_length + 10
         self.conv1 = tgnn.GATConv(
-            in_length, in_length, 5, concat=False, dropout=0.1, add_self_loops=True
+            in_length,
+            in_length,
+            5,
+            concat=False,
+            dropout=dropout_rate,
+            add_self_loops=True,
         )
         self.conv2 = tgnn.GATConv(
             in_length, in_length, 5, concat=False, add_self_loops=True
@@ -84,20 +92,22 @@ class JCIGraphAttentionNet(ChebaiBaseNet):
             nn.LeakyReLU(),
             nn.Linear(hidden_length, hidden_length),
             nn.LeakyReLU(),
-            nn.Linear(hidden_length, 500),
+            nn.Linear(hidden_length, self.out_dim),
         )
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, batch):
-        a = self.embedding(batch.x)
+        graph_data = batch["features"][0]
+        assert isinstance(graph_data, GraphData)
+        a = self.embedding(graph_data.x)
         a = self.dropout(a)
         a = torch.cat([a, torch.rand((*a.shape[:-1], 10)).to(self.device)], dim=1)
-        a = F.leaky_relu(self.conv1(a, batch.edge_index.long()))
-        a = F.leaky_relu(self.conv2(a, batch.edge_index.long()))
-        a = F.leaky_relu(self.conv3(a, batch.edge_index.long()))
-        a = F.leaky_relu(self.conv4(a, batch.edge_index.long()))
-        a = F.leaky_relu(self.conv5(a, batch.edge_index.long()))
+        a = F.leaky_relu(self.conv1(a, graph_data.edge_index.long()))
+        a = F.leaky_relu(self.conv2(a, graph_data.edge_index.long()))
+        a = F.leaky_relu(self.conv3(a, graph_data.edge_index.long()))
+        a = F.leaky_relu(self.conv4(a, graph_data.edge_index.long()))
+        a = F.leaky_relu(self.conv5(a, graph_data.edge_index.long()))
         a = self.dropout(a)
-        a = scatter_mean(a, batch.batch, dim=0)
+        a = scatter_mean(a, graph_data.batch, dim=0)
         a = self.output_net(a)
         return a
