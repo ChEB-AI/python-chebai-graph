5c619861cc615051944dee9cdeb698b525e7b4db
aditya0by0 <aditya0by0@gmail.com>
2025-04-21 16:53:57 +0200

    move fg constants to seperate file

---
+++ chebai_graph/preprocessing/fg_detection/fg_constants.py
@@ -0,0 +1,21 @@
+ATOM_NODE_LEVEL = "atom_node_lvl"
+FG_NODE_LEVEL = "fg_node_lvl"
+GRAPH_NODE_LEVEL = "graph_node_level"
+NODE_LEVEL = "node_level"
+NODE_LEVELS = {ATOM_NODE_LEVEL, FG_NODE_LEVEL, GRAPH_NODE_LEVEL}
+
+EDGE_LEVEL = "edge_level"
+WITHIN_ATOMS_EDGE = "within_atoms_lvl"
+WITHIN_FG_EDGE = "within_fg_lvl"
+ATOM_FG_EDGE = "atom_fg_lvl"
+FG_GRAPHNODE_LEVEL = "fg_graphNode_lvl"
+EDGE_LEVELS = {WITHIN_ATOMS_EDGE, WITHIN_FG_EDGE, ATOM_FG_EDGE, FG_GRAPHNODE_LEVEL}
+
+# fmt: off
+ELEMENTS = {"Ac", "Ag", "Al", "Am", "As", "At", "Au", "B", "Ba", "Be", "Bi", "Bk", "Br", "Ca", "Cd", "Ce", "Cf", "Cl",
+            "Cm", "Co", "Cr", "Cs", "Cu", "Dy", "Er", "Es", "Eu", "F", "Fe", "Fm", "Fr", "Ga", "Gd", "Ge", "He", "Hf",
+            "Hg", "Ho", "I", "In", "Ir", "K", "Kr", "La", "Li", "Lr", "Lu", "Md", "Mg", "Mn", "Mo", "N", "Na", "Nb",
+            "Nd", "Ne", "Ni", "Np", "O", "Os", "P", "Pa", "Pb", "Pd", "Pm", "Po", "Pr", "Pt", "Pu", "Ra", "Rb", "Re",
+            "Rh", "Rn", "Ru", "S", "Sb", "Sc", "Se", "Si", "Sm", "Sn", "Sr", "Ta", "Tb", "Tc", "Te", "Th", "Ti", "Tl",
+            "Tm", "U", "V", "W", "Xe", "Y", "Yb", "Zn", "Zr"}
+# fmt: on
