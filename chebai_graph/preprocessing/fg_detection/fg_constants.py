601d4c466640b1984ed6dec0482d8061ea46ea9c
aditya0by0 <aditya0by0@gmail.com>
2025-04-22 21:36:30 +0200

    move augmented properties to utils

--- chebai_graph/preprocessing/fg_detection/fg_constants.py
+++ chebai_graph/preprocessing/fg_detection/fg_constants.py
@@ -1,16 +1,3 @@
-ATOM_NODE_LEVEL = "atom_node_lvl"
-FG_NODE_LEVEL = "fg_node_lvl"
-GRAPH_NODE_LEVEL = "graph_node_level"
-NODE_LEVEL = "node_level"
-NODE_LEVELS = {ATOM_NODE_LEVEL, FG_NODE_LEVEL, GRAPH_NODE_LEVEL}
-
-EDGE_LEVEL = "edge_level"
-WITHIN_ATOMS_EDGE = "within_atoms_lvl"
-WITHIN_FG_EDGE = "within_fg_lvl"
-ATOM_FG_EDGE = "atom_fg_lvl"
-FG_GRAPHNODE_LEVEL = "fg_graphNode_lvl"
-EDGE_LEVELS = {WITHIN_ATOMS_EDGE, WITHIN_FG_EDGE, ATOM_FG_EDGE, FG_GRAPHNODE_LEVEL}
-
 # fmt: off
 ELEMENTS = {
     "Ac", "Ag", "Al", "Am", "As", "At", "Au", "B", "Ba", "Be", "Bi", "Bk", "Br", "Ca",
