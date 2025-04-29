52cc60a8393b264ed7ef6d6a13de898a7399d81b
aditya0by0 <aditya0by0@gmail.com>
2025-04-29 19:16:59 +0200

    right order of imports

--- chebai_graph/preprocessing/properties/__init__.py
+++ chebai_graph/preprocessing/properties/__init__.py
@@ -1,7 +1,8 @@
 # Formating is turned off here, because isort sorts the augmented properties imports in first order,
 # but it has to be imported after properties module, to avoid circular imports
-# fmt: off
-from .augmented_properties import *
+# This is because augmented properties module imports from properties module
+# isort: off
 from .properties import *
+from .augmented_properties import *
 
-# fmt: on
+# isort: on
