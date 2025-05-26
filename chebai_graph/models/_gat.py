f2cafbf082f8fb4275bcbc47d2529a6e5765c509
aditya0by0 <aditya0by0@gmail.com>
2025-05-26 13:45:05 +0200

    gat model pop config if used
    to avoid unnecessary kwargs getting passed to GAT

--- chebai_graph/models/_gat.py
+++ chebai_graph/models/_gat.py
@@ -8,33 +8,25 @@ from .graph import GraphBaseNet
 
 
 class GATModelWrapper(GraphBaseNet):
+    NAME = "GATModel"
+
     def __init__(self, config: dict, **kwargs):
         super().__init__(**kwargs)
 
-        self._in_length = config["in_length"]
-        self._hidden_length = config["hidden_length"]
-        self._dropout_rate = config["dropout_rate"]
-        self._n_conv_layers = (
-            config["n_conv_layers"] if "n_conv_layers" in config else 3
-        )
-        self._n_linear_layers = (
-            config["n_linear_layers"] if "n_linear_layers" in config else 3
-        )
-        self._n_atom_properties = int(config["n_atom_properties"])
-        self._n_bond_properties = (
-            int(config["n_bond_properties"]) if "n_bond_properties" in config else 7
-        )
-        self._n_molecule_properties = (
-            int(config["n_molecule_properties"])
-            if "n_molecule_properties" in config
-            else 0
-        )
+        self._in_length = int(config.pop("in_length"))
+        self._hidden_length = int(config.pop("hidden_length"))
+        self._dropout_rate = float(config.pop("dropout_rate", 0.1))
+        self._n_conv_layers = int(config.pop("n_conv_layers", 3))
+        self._n_linear_layers = int(config.pop("n_linear_layers", 3))
+        self._n_atom_properties = int(config.pop("n_atom_properties", 0))
+        self._n_bond_properties = int(config.pop("n_bond_properties", 0))
+        self._n_molecule_properties = int(config.pop("n_molecule_properties", 0))
         self._gat = GAT(
             in_channels=self._in_length,
             hidden_channels=self._hidden_length,
             num_layers=self._n_conv_layers,
             dropout=self._dropout_rate,
-            **kwargs,
+            **config,
         )
 
         self._ffn_activation = F.elu
