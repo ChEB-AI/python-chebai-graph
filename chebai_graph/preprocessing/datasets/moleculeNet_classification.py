5fad440c214288e7d6ce4e5bcac4a17c58f62213
aditya0by0 <aditya0by0@gmail.com>
2026-07-16 21:31:45 +0200

    data classe for molecule net

---
+++ chebai_graph/preprocessing/datasets/moleculeNet_classification.py
@@ -0,0 +1,65 @@
+from chebai.preprocessing.datasets.molecule_classfication import (
+    BaceChem,
+    BBBPChem,
+    ClinToxChem,
+    HIVChem,
+    SiderChem,
+    Tox21Chem,
+)
+
+from chebai_graph.preprocessing.datasets.base import (
+    GraphPropAsPerNodeType,
+    GraphPropertiesMixIn,
+)
+from chebai_graph.preprocessing.reader.augmented_reader import (
+    AtomFGReader_WithFGEdges_WithGraphNode,
+)
+from chebai_graph.preprocessing.reader.reader import GraphPropertyReader
+
+
+class BaceChemDataset(GraphPropertiesMixIn, BaceChem):
+    READER = GraphPropertyReader
+
+
+class BaceChem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, BaceChem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
+
+
+class BBBPChemDataset(GraphPropertiesMixIn, BBBPChem):
+    READER = GraphPropertyReader
+
+
+class BBBPChem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, BBBPChem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
+
+
+class ClinToxChemDataset(GraphPropertiesMixIn, ClinToxChem):
+    READER = GraphPropertyReader
+
+
+class ClinToxChem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, ClinToxChem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
+
+
+class HIVChemDataset(GraphPropertiesMixIn, HIVChem):
+    READER = GraphPropertyReader
+
+
+class HIVChem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, HIVChem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
+
+
+class SiderChemDataset(GraphPropertiesMixIn, SiderChem):
+    READER = GraphPropertyReader
+
+
+class SiderChem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, SiderChem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
+
+
+class Tox21ChemDataset(GraphPropertiesMixIn, Tox21Chem):
+    READER = GraphPropertyReader
+
+
+class Tox21Chem_WFGE_WGN_AsPerNodeType(GraphPropAsPerNodeType, Tox21Chem):
+    READER = AtomFGReader_WithFGEdges_WithGraphNode
