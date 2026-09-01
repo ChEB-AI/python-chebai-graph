f83e84e37dab25abee351b66bfcdee4a702a37c1
sfluegel <simon.fluegel@uni-osnabrueck.de>
2026-09-01 11:37:11 +0200

    fix method name

--- chebai_graph/preprocessing/datasets/chebi.py
+++ chebai_graph/preprocessing/datasets/chebi.py
@@ -214,7 +214,7 @@ class DataPropertiesSetter(ChEBIOverX, ABC):
         self._setup_properties()
         super()._after_setup(**kwargs)
 
-    def _preprocess_smiles_for_pred(
+    def _preprocess_molecule_for_pred(
         self, idx, raw_data: str | Chem.Mol, model_hparams: Optional[dict] = None
     ) -> Optional[dict]:
         """Preprocess prediction data."""
